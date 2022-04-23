2e0122d2ed9b071e1d1af8b07833715e17433e4b
Haruspex <you@example.com>
2022-04-23 15:15:17 +0800

    Translator has been made! TODO: Visual features

--- app/src/main/java/com/example/myapplication/MyTranslator.java
+++ app/src/main/java/com/example/myapplication/MyTranslator.java
@@ -1,5 +1,12 @@
 package com.example.myapplication;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.nl.translate.TranslateLanguage;
 import com.google.mlkit.nl.translate.Translation;
 import com.google.mlkit.nl.translate.Translator;
@@ -11,6 +18,7 @@ import java.util.Objects;
 public class MyTranslator {
     private final String TAG = MyTranslator.class.getSimpleName();
     private final MyLanguageIdentification languageIdentification = new MyLanguageIdentification();
+
     // TODO: Convert returning type to string after creating the architecture of the project
     // Note: Don't forget about target language to translate as a parameter
     public void translate(Text text) {
@@ -23,5 +31,37 @@ public class MyTranslator {
                         .build();
         final Translator toTargetLanguageTranslator = Translation.getClient(options);
         // TODO: Further translation
+        // Downloading the model
+        DownloadConditions conditions = new DownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        toTargetLanguageTranslator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.i(TAG, "Model has been downloaded successfully!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "Downloading of model went wrong.");
+                    }
+                });
+        toTargetLanguageTranslator.translate(String.valueOf(text))
+                .addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        // TODO: Extract the translated text
+                        Log.i(TAG, "Translated successfully! Extracting the translated text.");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "Translation of the text went wrong.");
+                    }
+                });
+
     }
 }
